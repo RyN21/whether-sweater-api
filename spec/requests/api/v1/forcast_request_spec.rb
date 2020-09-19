a5fa1fc28ffb74f4b01b8c686d7e1fee9936b53a
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-09-18 21:13:03 -0600

    Add test for get forcast

--- spec/requests/api/v1/forcast_request_spec.rb
+++ spec/requests/api/v1/forcast_request_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 describe "Forcast API per location" do
   it "sends forcast with attributes" do
+    headers = { "CONTENT_TYPE" => "application/json" }
+    placename = 'Denver, CO'
 
+    get "/api/v1/forcast?location=#{placename}"
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    require "pry"; binding.pry
   end
 end
