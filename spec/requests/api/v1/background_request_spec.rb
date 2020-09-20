7c5a459563d3b4d6de7b1689117109000210f753
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-09-20 00:16:05 -0600

    Finish test for forcast request

---
+++ spec/requests/api/v1/background_request_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'Background request' do
+  it "sends back a selected image from API based on current location" do
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+
+  end
+end
--- spec/requests/api/v1/forcast_request_spec.rb
+++ spec/requests/api/v1/forcast_request_spec.rb
@@ -1,15 +1,35 @@
 require 'rails_helper'
 
-describe "Forcast API per location" do
+describe "Forcast request" do
   it "sends forcast with attributes" do
     headers = { "CONTENT_TYPE" => "application/json" }
-    placename = 'Denver, CO'
+    location = 'Denver, CO'
 
-    get "/api/v1/forecast?location=#{placename}"
+    get "/api/v1/forecast?location=#{location}"
     expect(response).to be_successful
 
     forecast = JSON.parse(response.body, symbolize_names: true)
-require "pry"; binding.pry
 
+    expect(forecast[:data][:id]).to be_nil
+    expect(forecast[:data][:type]).to eq('forecast')
+    expect(forecast[:data][:attributes][:current_temp]).to    be_truthy
+    expect(forecast[:data][:attributes][:weather_summary]).to be_truthy
+    expect(forecast[:data][:attributes][:weather_main]).to    be_truthy
+    expect(forecast[:data][:attributes][:high]).to            be_truthy
+    expect(forecast[:data][:attributes][:low]).to             be_truthy
+    expect(forecast[:data][:attributes][:feels_like]).to      be_truthy
+    expect(forecast[:data][:attributes][:humidity]).to        be_truthy
+    expect(forecast[:data][:attributes][:visibility]).to      be_truthy
+    expect(forecast[:data][:attributes][:uv_index]).to        be_truthy
+    expect(forecast[:data][:attributes][:sunrise]).to         be_truthy
+    expect(forecast[:data][:attributes][:sunset]).to          be_truthy
+    expect(forecast[:data][:attributes][:hourly_48]).to       be_truthy
+    expect(forecast[:data][:attributes][:daily_8]).to         be_truthy
+    expect(forecast[:data][:attributes][:hourly_48].length).to eq(48)
+    expect(forecast[:data][:attributes][:daily_8].length).to   eq(8)
+
+    expect(forecast[:data][:attributes][:minutely]).to        be_falsy
+    expect(forecast[:data][:attributes][:pressure]).to        be_falsy
+    expect(forecast[:data][:attributes][:dew_point]).to       be_falsy
   end
 end
