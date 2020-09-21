de9bd175f0dae617f97ee28ef9e3c7fb703059ec
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-09-20 21:07:49 -0600

    Update background request spec

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -10,14 +10,29 @@ describe 'Background request' do
 
     image = JSON.parse(response.body, symbolize_names: true)
 
+    parsed_json = {
+                    data: {
+                      type: "image",
+                      id: nil,
+                      image: {
+                        location: "Denver, CO",
+                        image_url: "https://images.unsplash.com/photo-1599408169542-620fc453382c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE2Njk4NX0",
+                        credit: {
+                          source: "unsplash.com", author: "andrewcoop", logo: "https://unsplash.com/apple-touch-icon.png"
+                        }
+                      }
+                    }
+                  }
+
+    expect(image).to eq(parsed_json)
     expect(image[:data][:type]).to eq('image')
-    expect(image[:data][:id]).to                      be_nil
-    expect(image[:data][:image]).to                   be_truthy
-    expect(image[:data][:image][:location]).to        be_truthy
-    expect(image[:data][:image][:image_url]).to       be_truthy
-    expect(image[:data][:image][:credit]).to          be_truthy
-    expect(image[:data][:image][:credit][:source]).to be_truthy
-    expect(image[:data][:image][:credit][:author]).to be_truthy
-    expect(image[:data][:image][:credit][:logo]).to   be_truthy
+    expect(image[:data][:id]).to be_nil
+    expect(image[:data][:image]).to be_truthy
+    expect(image[:data][:image][:location]).to eq("Denver, CO")
+    expect(image[:data][:image][:image_url]).to eq("https://images.unsplash.com/photo-1599408169542-620fc453382c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE2Njk4NX0")
+    expect(image[:data][:image][:credit]).to be_truthy
+    expect(image[:data][:image][:credit][:source]).to eq("unsplash.com")
+    expect(image[:data][:image][:credit][:author]).to eq("andrewcoop")
+    expect(image[:data][:image][:credit][:logo]).to eq("https://unsplash.com/apple-touch-icon.png")
   end
 end
