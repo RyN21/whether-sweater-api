c4c944b230efe82c93bb8fb6ce007f46b5424bcf
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-09-21 16:10:56 -0600

    Add login request and test for sad paths

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::SessionsController < ApplicationController
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      render json: UserSerializer.new(user)
+    elsif user == nil
+      render json: "User not found. Check if you entered the correct email or Sign up"
+      response.status = 401
+    elsif !user.authenticate(params[:password])
+      render json: "Password does not match. Please try again."
+      response.status = 401
+    end
+  end
 
+  private
+  def login_params
+    params.permit(:email, :api_key, :password)
   end
 end
--- spec/requests/api/v1/user/user_login_request_spec.rb
+++ spec/requests/api/v1/user/user_login_request_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 describe "User Logs in request" do
   it "can log in" do
-    user = create(:user, email: "test@email.com", password: "password")
+    user = create(:user, email: "test21@email.com")
     headers  = { "CONTENT_TYPE" => "application/json" }
     login_params = {email: user.email,
-                   password: user.password}
+                    password: user.password}
 
     post "/api/v1/sessions", params: JSON.generate(login_params), headers: headers
     expect(response).to be_successful
 
     user_parse = JSON.parse(response.body, symbolize_names: true)
 
-    user = User.last
     body =  {
       "data": {
         "type": "user",
@@ -24,9 +23,37 @@ describe "User Logs in request" do
       }
     }
 
-    expect(response.status).to eq(201)
+    expect(response.status).to eq(200)
     expect(user_parse).to eq(body)
-    expect(user.email).to eq(user_params[:email])
+    expect(user.email).to eq(login_params[:email])
     expect(user_parse).to eq(body)
   end
+end
+
+describe "Unsuccessful Requests" do
+  it "tests for unsuccessful requests: Incorrect email" do
+    user = create(:user, email: "test21@email.com")
+    headers  = { "CONTENT_TYPE" => "application/json" }
+    login_params = {email: "wrongEmail@email.com",
+      password: user.password}
+
+      post "/api/v1/sessions", params: JSON.generate(login_params), headers: headers
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(401)
+      expect(response.body).to eq("User not found. Check if you entered the correct email or Sign up")
+    end
+
+    it "tests for unsuccessful requests: Incorrect password" do
+      user = create(:user, email: "test21@email.com")
+      headers  = { "CONTENT_TYPE" => "application/json" }
+      login_params = {email: user.email,
+        password: "wrongPassword"}
+
+        post "/api/v1/sessions", params: JSON.generate(login_params), headers: headers
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(401)
+        expect(response.body).to eq("Password does not match. Please try again.")
+      end
 end
