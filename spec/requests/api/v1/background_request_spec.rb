456f97a0b9ae5deefce535a375bc30c096cc2caf
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-09-20 20:59:26 -0600

    Fix backgroung test typo

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -10,14 +10,14 @@ describe 'Background request' do
 
     image = JSON.parse(response.body, symbolize_names: true)
 
-    expect(forecast[:data][:type]).to eq('image')
-    expect(forecast[:data][:id]).to                      be_nil
-    expect(forecast[:data][:image]).to                   be_truthy
-    expect(forecast[:data][:image][:location]).to        be_truthy
-    expect(forecast[:data][:image][:image_url]).to       be_truthy
-    expect(forecast[:data][:image][:credit]).to          be_truthy
-    expect(forecast[:data][:image][:credit][:source]).to be_truthy
-    expect(forecast[:data][:image][:credit][:author]).to be_truthy
-    expect(forecast[:data][:image][:credit][:logo]).to   be_truthy
+    expect(image[:data][:type]).to eq('image')
+    expect(image[:data][:id]).to                      be_nil
+    expect(image[:data][:image]).to                   be_truthy
+    expect(image[:data][:image][:location]).to        be_truthy
+    expect(image[:data][:image][:image_url]).to       be_truthy
+    expect(image[:data][:image][:credit]).to          be_truthy
+    expect(image[:data][:image][:credit][:source]).to be_truthy
+    expect(image[:data][:image][:credit][:author]).to be_truthy
+    expect(image[:data][:image][:credit][:logo]).to   be_truthy
   end
 end
