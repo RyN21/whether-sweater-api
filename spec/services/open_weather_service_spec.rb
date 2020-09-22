343a868575a1486c6bf533e31577793b4f086dd6
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-09-22 12:12:33 -0600

    Clean up code

--- spec/services/open_weather_service_spec.rb
+++ spec/services/open_weather_service_spec.rb
@@ -6,5 +6,10 @@ describe 'Open Weather Service' do
     result   = OpenWeatherService.new.onecall(location)
 
     expect(result[:timezone]).to eq("America/Denver")
+    expect(result[:current]).to be_truthy
+    expect(result[:daily]).to be_truthy
+    expect(result[:hourly]).to be_truthy
+    expect(result[:minutely]).to be_nil
+    expect(result[:alerts]).to be_nil
   end
 end
