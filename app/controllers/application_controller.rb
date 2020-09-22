6577793231a087b1aa52112245dcb68cb5981bfd
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-09-22 12:00:19 -0600

    Remove current user method. Did not need to authenticate api key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::API
-  include ActionController::Helpers
-  helper_method :current_user
-
-  def current_user
-    require "pry"; binding.pry
-    User.find(session[:user_id]) if session[:user_id]
-  end
 end
